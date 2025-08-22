91d55382305194a26f40f25fbe6a7d752bed338f
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-08-22 17:51:52 +0530

    base url changed

--- src/helper/baseurl.js
+++ src/helper/baseurl.js
@@ -2,7 +2,7 @@ import axios from "axios";
  
 // Create an axios instance with your base URL
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://trakingbackend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
