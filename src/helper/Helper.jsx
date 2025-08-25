87370c0b21a4f5d658b678b5924ad9e515438271
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-08-25 11:20:43 +0530

    url changed

--- src/helper/Helper.jsx
+++ src/helper/Helper.jsx
@@ -1,4 +1,5 @@
-const baseurl = "https://trakingbackend.onrender.com";
+// const baseurl = "https://trakingbackend.onrender.com";
+const baseurl = "https://offer.mvmtracking.com";
 // const baseurl="https://topsbackend.onrender.com"
 // const baseurl="https://topsbackend-1.onrender.com"
 // const baseurl="https://api.top5shots.com"
--- src/helper/baseurl.js
+++ src/helper/baseurl.js
@@ -2,7 +2,8 @@ import axios from "axios";
  
 // Create an axios instance with your base URL
 const api = axios.create({
-  baseURL: "https://trakingbackend.onrender.com/api",
+  // baseURL: "https://trakingbackend.onrender.com/api",
+  baseURL: "https://offer.mvmtracking.com/api",
   headers: {
     "Content-Type": "application/json",
   },
