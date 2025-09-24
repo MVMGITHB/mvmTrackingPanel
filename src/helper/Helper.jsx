fc5f4725b41998ba8dc703a2953313ff72383eff
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-09-24 19:05:15 +0530

    url updated

--- src/helper/Helper.jsx
+++ src/helper/Helper.jsx
@@ -1,5 +1,5 @@
 
-// const baseurl = "https://offer.mvmtracking.com";
-const baseurl = "http://localhost:5011";
+const baseurl = "https://offer.mvmtracking.com";
+// const baseurl = "http://localhost:5011";
 
 export { baseurl };
