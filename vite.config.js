819dde26a351a5ff83ea21faca2f85f2de0e245a
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-12-01 11:02:27 +0530

    build optimised

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,51 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+    
+//     tailwindcss(),
+  
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    
+  plugins: [
+    react(),
     tailwindcss(),
-  
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Core libraries
+          react: ['react', 'react-dom'],
+
+          // Large UI libraries
+          antd: ['antd'],
+
+          // Charts
+          recharts: ['recharts'],
+
+          // Animation
+          motion: ['framer-motion'],
+
+          // Icons (big bundle)
+          icons: ['react-icons'],
+
+          // Router
+          router: ['react-router-dom'],
+        }
+      }
+    },
+    // Optional: raise warning limit
+    chunkSizeWarningLimit: 1000
+  }
 })
